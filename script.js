4a2547feb34eb7875a09e37afeb3c825c6abf8d2
Bunks <busk_1@yahoo.com>
2020-12-16 18:32:10 -0500

    made script js and psuedo code

---
+++ script.js
@@ -0,0 +1,14 @@
+//psuedo code
+
+// make the html dynamically
+//--for loop making elements
+//-- 3 step process for making 3 columns
+
+// on load access local storage to retrieve text boxes
+
+// make the text boxes get saved to local storage when the save button is clicked
+
+// check the time to reassess the color schemes of the rows
+// -- use day js to check the hour and check - do a if statement on class creation to change color
+
+var arrayOfTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
