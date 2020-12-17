ddf7c7984858b8f21796fd9cc49f9d23585899ac
Bunks <busk_1@yahoo.com>
2020-12-16 23:25:33 -0500

    day planner is finished with no bugs found (so far)

--- script.js
+++ script.js
@@ -12,17 +12,15 @@
 // -- use day js to check the hour and check - do a if statement on class creation to change color
 
 //basic array for how many hours are in our day to iterate over
-var arrayOfTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+$(document).ready(function () {
+  var arrayOfTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-// setting the time and formatting and setting currentday id to this.
-var hour = dayjs().format("HH");
-var day = dayjs().format("MMMM DD YYYY hh:mm:ss A");
-$("#currentDay").text(day);
-console.log(hour);
+  // setting the time and formatting and setting currentday id to this.
+  var hour = dayjs().format("HH");
+  var day = dayjs().format("MMMM DD YYYY hh:mm:ss A");
+  $("#currentDay").text(day);
 
-
-for (let i = 0; i < arrayOfTime.length; i++) {
-
+  for (let i = 0; i < arrayOfTime.length; i++) {
     //making the row to append things onto and this appends to container
     let rowOfTime = $("<div></div>");
     rowOfTime.attr("class", "row time-block");
@@ -32,32 +30,48 @@ for (let i = 0; i < arrayOfTime.length; i++) {
     let divOfTime = $("<div></div>");
     divOfTime.attr("class", "hour col-1");
     if (i < 3) {
-        divOfTime.text(arrayOfTime[i] + " AM");
+      divOfTime.text(arrayOfTime[i] + " AM");
     } else if (i === 3) {
-        divOfTime.text(arrayOfTime[i] + " PM");
+      divOfTime.text(arrayOfTime[i] + " PM");
     } else {
-        divOfTime.text(arrayOfTime[i] - 12 + " PM");
+      divOfTime.text(arrayOfTime[i] - 12 + " PM");
     }
     rowOfTime.append(divOfTime);
 
-    //making the text areas and appending to row 
+    //making the text areas and appending to row
     let textOfTime = $("<textarea>");
     if (arrayOfTime[i] < hour) {
-        textOfTime.attr("class", "past description col-10");
+      textOfTime.attr("class", "past description col-10");
     } else if (arrayOfTime[i] > hour) {
-        textOfTime.attr("class", "future description col-10");
+      textOfTime.attr("class", "future description col-10");
     } else {
-        textOfTime.attr("class", "present description col-10");
+      textOfTime.attr("class", "present description col-10");
     }
-    
+    // retrieving local storage and adding arrayoftime[i] to the key to get it from the correct area.
+    var textRetrieval = localStorage.getItem("textAreaData" + arrayOfTime[i]);
+    if (textRetrieval === null) {
+        textRetrieval = "";
+    } else {
+        textRetrieval = JSON.parse(textRetrieval);
+    }
+    textOfTime.val(textRetrieval);
     rowOfTime.append(textOfTime);
 
     // making the button area and appending to row
-    let buttonOfTime = $("<button>");
-    buttonOfTime.attr("class", "saveBtn col-1");
+    let buttonOfTime = $("<button></button>");
+    buttonOfTime.attr("class", "saveBtn col-1 fas fa-save");
+    buttonOfTime.data("data-index", arrayOfTime[i]);
     rowOfTime.append(buttonOfTime);
-
-    
-}
-
-$()
+  }
+  //
+  $(".saveBtn").on("click", function (e) {
+    //get local data of the text area
+    var textStoring = $(this).prev().val();
+    // figure out how to set the data equal to the row
+    // using $this.data to retrieve a number and attach that to the key to get a local data for each button.
+    var buttonNumber = $(this).data("data-index");
+    console.log(buttonNumber);
+    //set local data of the text
+    localStorage.setItem("textAreaData" + buttonNumber, JSON.stringify(textStoring));
+  });
+});
