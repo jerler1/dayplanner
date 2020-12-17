f4054011a752cfe2f9405685bf2772ecfbbc5d4c
Bunks <busk_1@yahoo.com>
2020-12-16 20:04:20 -0500

    added color funcality to the text area in relation to time, and added a time at the top

--- script.js
+++ script.js
@@ -10,8 +10,16 @@
 
 // check the time to reassess the color schemes of the rows
 // -- use day js to check the hour and check - do a if statement on class creation to change color
-console.log("hi")
+
+//basic array for how many hours are in our day to iterate over
 var arrayOfTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+// setting the time and formatting and setting currentday id to this.
+var hour = dayjs().format("HH");
+var day = dayjs().format("MMMM DD YYYY hh:mm:ss A");
+$("#currentDay").text(day);
+console.log(hour);
+
 
 for (let i = 0; i < arrayOfTime.length; i++) {
 
@@ -34,7 +42,14 @@ for (let i = 0; i < arrayOfTime.length; i++) {
 
     //making the text areas and appending to row 
     let textOfTime = $("<textarea>");
-    textOfTime.attr("class", "description col-10");
+    if (arrayOfTime[i] < hour) {
+        textOfTime.attr("class", "past description col-10");
+    } else if (arrayOfTime[i] > hour) {
+        textOfTime.attr("class", "future description col-10");
+    } else {
+        textOfTime.attr("class", "present description col-10");
+    }
+    
     rowOfTime.append(textOfTime);
 
     // making the button area and appending to row
@@ -44,3 +59,5 @@ for (let i = 0; i < arrayOfTime.length; i++) {
 
     
 }
+
+$()
